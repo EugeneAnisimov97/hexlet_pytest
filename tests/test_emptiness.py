c5d6244e8f46a35f9c80117332708c1f64e19c18
Eugene Anisimov <zero0061@mail.ru>
2024-03-20 18:32:21 +0200

    add workflow

--- tests/test_emptiness.py
+++
@@ -1,8 +0,0 @@
-def test_emptiness():
-    stack = []
-    assert not stack
-    stack.append('one')
-    assert bool(stack)  # not not stack
-
-    stack.pop()
-    assert not stack
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,3 +1,7 @@
+import pytest
+from hexlet_pytest.stack import stack
+
+
 def test_stack():
     stack = []
     # Добавляем два элемента в стек и затем извлекаем их
@@ -23,3 +27,24 @@ def test_stack2():
     
     stack.pop()
     assert stack.pop() == 'one'
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # если внутри блока вызовется исключение, то тест будет пройден
+    with pytest.raises(IndexError):
+        stack.pop()
+        
+        
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
+
+
+
